656158eb1ca5e752b8c7a8eb8d34fab667e1bbe8
Mark Oliver <moliv155@fiu.edu>
2023-06-03 00:46:24 -0400

    remove comments

--- routes/books.js
+++ routes/books.js
@@ -6,7 +6,6 @@ const books = require('../services/books');
 router.get('/', async function(req, res, next) {
     try {
         const { ISBN } = req.query
-        console.log(`ISBN = ${ISBN}`)
         res.json(await books.getBook(ISBN));
     } catch (err) {
         console.error(`Error while getting Book by ISBN `, err.message);
--- services/books.js
+++ services/books.js
@@ -4,7 +4,6 @@ const config = require('../config');
 
 
 async function getBook(isbn = 1){
-    console.log(`book.js isbn ${isbn}`)
     const row = await db.query(
         `SELECT id, ISBN, Name, ROUND(Price,2) as Price, ROUND(Rating, 4) as Rating, GenreId, PublisherId, Year, Copies, Description FROM books.books b where b.ISBN=${isbn}`
     );
