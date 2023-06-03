8e316c9db9848a081ed4e15790b3b73163ddeb89
Mark Oliver <moliv155@fiu.edu>
2023-06-03 07:03:19 -0400

    improve book_authors

--- book_authors.sql
+++ book_authors.sql
@@ -1,12 +1,12 @@
-use bookIBSN;
+use books;
 DROP TABLE book_authors;
 create table book_authors (
  BookId INT NOT NULL,
--- 	FOREIGN KEY(BookId)
---     REFERENCES bookIBSN(id)
---     ON DELETE CASCADE,
- AuthorId INT NOT NULL
--- 	FOREIGN KEY(AuthorId)
---     REFERENCES authors(id)
---     ON DELETE CASCADE
+	FOREIGN KEY(BookId)
+    REFERENCES books(id)
+    ON DELETE CASCADE,
+ AuthorId INT NOT NULL,
+	FOREIGN KEY(AuthorId)
+    REFERENCES authors(id)
+    ON DELETE CASCADE
 );
