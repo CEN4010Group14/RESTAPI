c862c1a15c82b59925df56dff7f3490afe3f7812
Mark Oliver <moliv155@fiu.edu>
2023-06-03 00:33:09 -0400

    round Price and Rating

--- services/books.js
+++ services/books.js
@@ -1,10 +1,12 @@
 const db = require('./db');
 const config = require('../config');
 
+
+
 async function getBook(isbn = 1){
     console.log(`book.js isbn ${isbn}`)
     const row = await db.query(
-        `SELECT * FROM books.books b where b.ISBN=${isbn}`
+        `SELECT id, ISBN, Name, ROUND(Price,2) as Price, ROUND(Rating, 4) as Rating, GenreId, PublisherId, Year, Copies, Description FROM books.books b where b.ISBN=${isbn}`
     );
 
     return {
