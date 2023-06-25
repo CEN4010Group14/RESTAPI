2852aea734ad8dbddfa78448b700bbfdbe2e2d78
Mark Oliver <moliv155@fiu.edu>
2023-06-25 10:43:06 -0400

    better error message for IBSN not found

--- routes/bookIBSN.js
+++ routes/bookIBSN.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const books = require('../services/books');
+const {fa} = require("faker/lib/locales");
 
 /* GET book by ISBN */
 router.get('/', async function(req, res, next) {
     try {
         const { ISBN } = req.query
-        res.json(await books.getBookByISBN(ISBN));
+        const result = await books.getBookByISBN(ISBN);
+        const s = JSON.stringify(result);
+        // console.log(`s: ${s}`)
+        if (s === '{"row":[]}') return res.json({success: false, result: `ISBN ${ISBN} not found`});
+        return res.json(result);
     } catch (err) {
         console.error(`Error while getting Book by ISBN `, err.message);
         next(err);
