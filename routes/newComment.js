52ac6519f6ec2d7ada53f37994f2712bfe87a4c3
ipalencia <43596353+ipalencia@users.noreply.github.com>
2023-06-27 02:52:19 -0400

    deleted unecessary comments

--- routes/newComment.js
+++ routes/newComment.js
@@ -2,17 +2,6 @@ const express = require('express');
 const router = express.Router();
 const commentsService = require('../services/comments');
 
-/*router.post('/', async (req, res, next) => {
-  try {
-    const { Comment, Datestamp, UserId, BookId } = req.body;
-    await commentsService.createComment(Comment, Datestamp, UserId, BookId);
-    res.sendStatus(200);
-  } catch (err) {
-    console.error('Error creating comment:', err);
-    res.sendStatus(500);
-  }
-});*/
-
 router.post('/', async function(req, res, next) {
   try {
     const { Comment, UserId, BookId } = req.body;
