384731a5f11b0886373aa7dcf95ea47e4e204f5c
Chukwuma <cnzer002@fiu.edu>
2023-07-11 22:47:38 -0400

    updates

--- routes/expre.js
+++ routes/expre.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const services = require('../services/expre');
-/* GET */
-router.get('/', async function(req, res, next) {
-    try {
-        res.json(await service.getByid(req.query.expre));
-    } catch (err) {
-        console.error(`Error while getting genres `, err.message);
-        next(err);
+
+// Create a User
+router.post('/', async (req, res) => {
+  try {
+    const user = req.body; // Assuming the request body contains the user object
+    await services.createUser(user);
+    res.sendStatus(201);
+  } catch (err) {
+    console.error('Error creating user:', err);
+    res.status(500).send('Error creating user');
+  }
+});
+
+// GET /api/users/:username
+router.get('/:username', async (req, res, next) => {
+  try {
+    const username = req.params.username;
+    const user = await services.getUserByUsername(username);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
     }
+
+    res.json(user);
+  } catch (err) {
+    console.error('Error retrieving user:', err);
+    res.status(500).json({ message: 'Error retrieving user' });
+  }
+
 });
 // POST /api/credit-cards
 router.post('/', async (req, res) => {
@@ -23,4 +44,29 @@ router.post('/', async (req, res) => {
 
     }
   });
+  // PUT /api/users/:username
+
+router.put('/:username', async (req, res) => {
+
+  try {
+
+    const username = req.params.username;
+
+    const updatedFields = req.body; // Assuming the request body contains the updated fields
+
+ 
+
+    await services.updateUser(username, updatedFields);
+
+    res.sendStatus(204);
+
+  } catch (err) {
+
+    console.error('Error updating user:', err);
+
+    res.status(500).send('Error updating user');
+
+  }
+
+});
 module.exports = router;
