0b6f5d59fb7f92c835b211ccfd191b5d1dcbcf33
xeteru <120993112+xeteru@users.noreply.github.com>
2023-07-17 11:01:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
-const userRouter = require("./sprint 2/routes/users")
-const cartRouter = require("./sprint 2/routes/cart")
+const userRouter = require("./routes/user")
+const cartRouter = require("./routes/cart")
 
 app.use(express.static("public"))
 app.use(logger)
 
 app.use("/users", userRouter)
-app.use("/users/:id/shopping-cart", cartRouter)
+app.use("/users", cartRouter)
 
 function logger(req, res, next) {
     console.log(req.originalUrl)
